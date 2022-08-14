a8f0d98a62af192e969b2bd97f509d581ad3c3ed
Shakesuki <angerika.0627@gmail.com>
2022-08-14 23:20:40 +0900

    ナビゲーションからJobpediaに変更

--- jobpedia_main.py
+++ jobpedia_main.py
@@ -48,44 +48,38 @@ def contents(df2):
     st.markdown(f"{df2['discourage'].to_string(index=False)}")
 
 def main():
-    sheet_url = st.secrets["private_gsheets_url"]
-    wholedata = run_query(f'SELECT * FROM "{sheet_url}"')
-
-    df = pd.DataFrame(wholedata)
-    #df.reset_index()
+     #df.reset_index()
     #print(df["category"])
 
-    options = st.sidebar.radio('ナビゲーション', options = ['ホームへ戻る','投稿を読む'])
-
-    st.sidebar.title('投稿を読む')
-
-    #select level 1: job category
-    jobclist = df["category"]
-    jobclist = jobclist[~jobclist.duplicated()] 
-    
-    jobc = st.sidebar.selectbox("職業カテゴリ:", jobclist)
-    df1 = df[df["category"] == jobc]
-
-
-    #select level 2: individual page
-    jobnamelist = df1["jobname"]
-    jobname = st.sidebar.selectbox("職業名:", jobnamelist)
-
-
-
-    #Print page
-    # import pdb;pdb.set_trace()
-
-    df2 = df1[df1["jobname"] == jobname]
-
-
-# st.write(df2["jobname"])
+    options = st.sidebar.radio('Jobpedia', options = ['ホームへ戻る','投稿を読む'])
 
 
     if options == 'ホームへ戻る':
         home()
     else:
+        sheet_url = st.secrets["private_gsheets_url"]
+        wholedata = run_query(f'SELECT * FROM "{sheet_url}"')
+
+        df = pd.DataFrame(wholedata)
+  
+        
+        st.sidebar.title('投稿を読む')
+
+        #select level 1: job category
+        jobclist = df["category"]
+        jobclist = jobclist[~jobclist.duplicated()] 
+        
+        jobc = st.sidebar.selectbox("職業カテゴリ:", jobclist)
+        df1 = df[df["category"] == jobc]
+
+
+        #select level 2: individual page
+        jobnamelist = df1["jobname"]
+        jobname = st.sidebar.selectbox("職業名:", jobnamelist)
+
+        df2 = df1[df1["jobname"] == jobname]
         contents(df2)
+
 
 
 main()
